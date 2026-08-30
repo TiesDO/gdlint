5c14d46830cbb738558745917a9e5d37c0819c37
Ties den Ouden <denouden.ties@gmail.com>
2026-08-31 00:25:53 +0200

    feat(lsp): setup language server protocol capable server
    This module contains a Server struct that can runs the linting rules based on lsp messages

--- go.mod
+++ go.mod
@@ -7,11 +7,15 @@ require (
 	github.com/smacker/go-tree-sitter v0.0.0-20240827094217-dd81d9e9be82
 	github.com/spf13/cobra v1.10.2
 	github.com/stretchr/testify v1.12.1
+	go.lsp.dev/jsonrpc2 v1.0.1
+	go.lsp.dev/protocol v1.0.1
+	go.lsp.dev/uri v1.0.1
 )
 
 require (
+	github.com/go-json-experiment/json v0.0.0-20260623181947-01eb4420fa68 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/mattn/go-pointer v0.0.1 // indirect
-	github.com/spf13/pflag v1.0.9 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
 	go.yaml.in/yaml/v3 v3.0.5 // indirect
 )
---
+++ lsp/lsp.go
@@ -0,0 +1,204 @@
+package lsp
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"log"
+	"os"
+
+	"github.com/TiesDO/gdlint/rules"
+	jrpc "go.lsp.dev/jsonrpc2"
+	"go.lsp.dev/protocol"
+)
+
+const LSP_SOURCE string = "gdlint"
+
+type stdio struct {
+	io.Reader
+	io.Writer
+	io.Closer
+}
+
+type Server struct {
+	stream jrpc.Stream
+	conn   jrpc.Conn
+	logger *log.Logger
+}
+
+func NewServer(stream_in *os.File, stream_out *os.File, logger *log.Logger) Server {
+	if logger == nil {
+		logger = log.New(io.Discard, "", 0)
+	}
+
+	server := Server{
+		logger: logger,
+	}
+
+	server.stream = jrpc.NewStream(stdio{
+		Reader: stream_in,
+		Writer: stream_out,
+		Closer: stream_in,
+	})
+
+	server.conn = jrpc.NewConn(server.stream)
+
+	return server
+}
+
+func (s *Server) Run(ctx context.Context) {
+	s.logger.Println("starting listener")
+	s.conn.Go(ctx, s.handler)
+	<-s.conn.Done()
+}
+
+func (s *Server) handler(ctx context.Context, req *jrpc.Request) (any, error) {
+	s.logger.Println("handler called with method", req.Method())
+
+	switch req.Method() {
+	case protocol.MethodInitialize:
+		return s.methodInitialize()
+	case protocol.MethodTextDocumentDidOpen:
+		return s.methodTextDocumentDidOpen(ctx, req)
+	case protocol.MethodTextDocumentDidChange:
+		return s.methodTextDocumentDidChange(ctx, req)
+	case protocol.MethodShutdown:
+		return nil, nil
+	}
+
+	return nil, nil
+}
+
+func (s *Server) methodInitialize() (any, error) {
+	openClose := true
+	syncKind := protocol.TextDocumentSyncKindFull
+
+	return protocol.InitializeResult{
+		Capabilities: protocol.ServerCapabilities{
+			TextDocumentSync: &protocol.TextDocumentSyncOptions{
+				OpenClose: &openClose,
+				Change:    &syncKind,
+			},
+		},
+	}, nil
+}
+
+func (s *Server) methodTextDocumentDidOpen(ctx context.Context, req *jrpc.Request) (any, error) {
+	var params protocol.DidOpenTextDocumentParams
+
+	if err := json.Unmarshal(req.Params(), &params); err != nil {
+		s.logger.Printf("failed to unmarshal DidOpen params: %v\n", err)
+		return nil, err
+	}
+
+	source := []byte(params.TextDocument.Text)
+
+	diagnostics, err := s.runDiagnostics(ctx, source)
+
+	if err != nil {
+		s.logger.Printf("failed to run diagnostics: %v\n", err)
+		return nil, err
+	}
+
+	if len(diagnostics) == 0 {
+		s.logger.Printf("no diagnostics found\n")
+		return diagnostics, nil
+	}
+
+	uri := params.TextDocument.URI
+
+	s.logger.Printf("notifying client of %d diagnostics\n", len(diagnostics))
+
+	err = s.conn.Notify(ctx, protocol.MethodTextDocumentPublishDiagnostics, protocol.PublishDiagnosticsParams{
+		URI:         uri,
+		Diagnostics: diagnostics,
+	})
+
+	if err != nil {
+		s.logger.Printf("failed to notify diagnostics: %v\n", err)
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func (s *Server) methodTextDocumentDidChange(ctx context.Context, req *jrpc.Request) (any, error) {
+	var params protocol.DidChangeTextDocumentParams
+
+	if err := json.Unmarshal(req.Params(), &params); err != nil {
+		return nil, err
+	}
+
+	if len(params.ContentChanges) == 0 {
+		return nil, nil
+	}
+
+	full_change_event, ok := params.ContentChanges[0].(*protocol.TextDocumentContentChangeWholeDocument)
+
+	if !ok {
+		return nil, errors.New("expected only whole document change events")
+	}
+
+	source := []byte(full_change_event.Text)
+
+	diagnostics, err := s.runDiagnostics(ctx, source)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(diagnostics) == 0 {
+		s.logger.Printf("no diagnostics found\n")
+		return diagnostics, nil
+	}
+
+	uri := params.TextDocument.URI
+
+	err = s.conn.Notify(ctx, protocol.MethodTextDocumentPublishDiagnostics, protocol.PublishDiagnosticsParams{
+		URI:         uri,
+		Diagnostics: diagnostics,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func (s *Server) runDiagnostics(ctx context.Context, source []byte) ([]protocol.Diagnostic, error) {
+	runner := rules.NewRuleRunner(&rules.DefaultRuleRegistry, source)
+	warnings, err := runner.RunRules(rules.DefaultRuleRegistry.RuleNames(), ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(warnings) == 0 {
+		return []protocol.Diagnostic{}, nil
+	}
+
+	diagnostics := make([]protocol.Diagnostic, len(warnings))
+
+	for i, warning := range warnings {
+		diagnostics[i] = protocol.Diagnostic{
+			Range: protocol.Range{
+				Start: protocol.Position{
+					Line:      uint32(warning.StartLine),
+					Character: uint32(warning.StartChar),
+				},
+				End: protocol.Position{
+					Line:      uint32(warning.EndLine),
+					Character: uint32(warning.EndChar),
+				},
+			},
+			Code:     protocol.String(warning.Offense),
+			Source:   protocol.NewOptional(LSP_SOURCE),
+			Message:  protocol.String(warning.Message),
+			Severity: protocol.DiagnosticSeverityWarning,
+		}
+	}
+
+	return diagnostics, nil
+}
