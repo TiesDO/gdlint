54fc324bc5822224f696b03ae734b77a50864f99
sdenouden <sdodenouden@gmail.com>
2026-08-31 17:20:01 +0200

    test(rules): add naming convention file
    Added a file to check all gdscript naming conventiens when running tests

---
+++ fixtures/scripts/naming_conventions.gd
@@ -0,0 +1,20 @@
+class_name foo
+
+const CORRECT_CASE_CONST = 200
+const CORRECT_CASE_CONST_2 = 300
+const snake_case_const = 0
+const camelCaseConst = ""
+const PascalCaseConst = 1
+
+class bar:
+	const CORRECT_CASE_CONST = 200
+	const CORRECT_CASE_CONST_2 = 300
+	const snake_case_const = 0
+	const camelCaseConst = ""
+	const PascalCaseConst = 1
+
+class Bazz:
+	pass
+
+class BAR_TEST:
+	pass
