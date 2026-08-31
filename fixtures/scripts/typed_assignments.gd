acebaaa842f7216c0b250c7c0d412979004e6cd9
sdenouden <sdodenouden@gmail.com>
2026-08-31 22:08:01 +0200

    feat(rules): add untyped_const_statement rule
    Check's if constants have no type or are declared as 'inferred'

--- fixtures/scripts/typed_assignments.gd
+++ fixtures/scripts/typed_assignments.gd
@@ -2,6 +2,10 @@ var a := 5
 var b: int = 5
 var c = 5
 
+const VALID_CONST_TYPE_ASSIGN: String = "valid"
+const INFERRED_CONST_TYPE := "inferred"
+const INVALID_CONST_TYPE_ASSIGN = 0
+
 func foo() -> void:
 	var x := 6
 	var y: int = 6
