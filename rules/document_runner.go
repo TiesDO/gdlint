d64b28b38a270ef6568f9ed78193b860dec42fa5
Ties den Ouden <denouden.ties@gmail.com>
2026-09-02 23:58:55 +0200

    feat(rules): add DocumentRunner struct to replace RuleRunner
    This replaces the RuleRunner and works directly with Document structs. The reasoning is that for a document parsing is not owned nor triggered by the runner, instead the active ruleset is held by the runner. When the runner does not hold any state of the documents it checks we can re-use the same runner for as many documents as needed until a ruleset changes.

---
+++ rules/document_runner.go
@@ -0,0 +1,135 @@
+package rules
+
+import (
+	"fmt"
+	"strings"
+)
+
+type DocumentRunner struct {
+	registry   *RuleRegistry
+	nodeRules  []*NodeRule
+	matchRules []*MatchRule
+}
+
+func NewDocumentRunner(registry *RuleRegistry) *DocumentRunner {
+	runner := DocumentRunner{
+		registry:   registry,
+		nodeRules:  []*NodeRule{},
+		matchRules: []*MatchRule{},
+	}
+
+	return &runner
+}
+
+func (r *DocumentRunner) SetRules(rules []string) error {
+	r.nodeRules = make([]*NodeRule, 0)
+	r.matchRules = make([]*MatchRule, 0)
+
+	for _, ruleName := range rules {
+		rule := r.registry.GetByName(ruleName)
+
+		if rule == nil {
+			return fmt.Errorf("failed to find rule '%s'", ruleName)
+		}
+
+		if nodeRule, ok := rule.(*NodeRule); ok {
+			r.nodeRules = append(r.nodeRules, nodeRule)
+		} else if matchRule, ok := rule.(*MatchRule); ok {
+			r.matchRules = append(r.matchRules, matchRule)
+		} else {
+			return fmt.Errorf("rule '%s' is of an unsupported type", ruleName)
+		}
+	}
+
+	return nil
+}
+
+func (r *DocumentRunner) CheckDocument(doc *Document) ([]Warning, error) {
+	warnings := []Warning{}
+
+	nodeWarnings, err := r.runNodeRules(doc)
+
+	if err != nil {
+		return warnings, err
+	}
+
+	warnings = append(warnings, nodeWarnings...)
+
+	matchWarnings, err := r.runMatchRules(doc)
+
+	if err != nil {
+		return warnings, err
+	}
+
+	warnings = append(warnings, matchWarnings...)
+
+	return warnings, nil
+}
+
+func (r *DocumentRunner) runNodeRules(doc *Document) ([]Warning, error) {
+	warnings := []Warning{}
+
+	for _, rule := range r.nodeRules {
+		foundWarnings, err := rule.Execute(doc.parser.tree.RootNode(), doc.source)
+
+		if err != nil {
+			return nil, err
+		}
+
+		warnings = append(warnings, foundWarnings...)
+	}
+
+	return warnings, nil
+}
+
+func (r *DocumentRunner) runMatchRules(doc *Document) ([]Warning, error) {
+	pattern, err := r.buildMatchRulePattern()
+
+	if err != nil {
+		return nil, err
+	}
+
+	matches, err := doc.Query(pattern)
+
+	warnings := []Warning{}
+
+	for {
+		match := matches.Next()
+
+		if match == nil {
+			break
+		}
+
+		rule := r.matchRules[match.PatternIndex]
+
+		foundWarnings, err := rule.Execute(match, doc.source)
+
+		if err != nil {
+			return nil, err
+		}
+
+		warnings = append(warnings, foundWarnings...)
+	}
+
+	return warnings, nil
+}
+
+func (r *DocumentRunner) buildMatchRulePattern() (string, error) {
+	var builder strings.Builder
+
+	for _, rule := range r.matchRules {
+		_, err := builder.Write(rule.pattern)
+
+		if err != nil {
+			return "", err
+		}
+
+		_, err = builder.WriteString("\n\n")
+
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return builder.String(), nil
+}
