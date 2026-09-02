cbb54e23e829b2fca063e3eeb5631d5f80a11004
sdenouden <sdodenouden@gmail.com>
2026-09-02 22:44:37 +0200

    feat(rules): add NewWarningFromNode() in rules
    By providing the node, a message, and an offense, you can now easily
    declare a warning struct making writing new rules more effortless.

--- rules/rule.go
+++ rules/rule.go
@@ -22,6 +22,18 @@ type Warning struct {
 	Offense string
 }
 
+func NewWarningFromNode(node sitter.Node, message string, offense string) *Warning {
+	return &Warning{
+		StartLine: int(node.StartPosition().Row),
+		StartChar: int(node.StartPosition().Column),
+		EndLine:   int(node.EndPosition().Row),
+		EndChar:   int(node.EndPosition().Column),
+
+		Message: message,
+		Offense: offense,
+	}
+}
+
 func (w *Warning) FullMessage() string {
 	return fmt.Sprintf("%d:%d (@%s) - %s", w.StartLine+1, w.StartChar+1, w.Offense, w.Message)
 }
