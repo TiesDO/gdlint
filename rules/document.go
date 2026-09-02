5fcb1f959044734f98004f9c407ba880e332771f
Ties den Ouden <denouden.ties@gmail.com>
2026-09-02 23:56:02 +0200

    feat(rules): add Document struct to hold document state
    This will later allow us to control concurrent document parsing when LSP requests overlap for the same document.

---
+++ rules/document.go
@@ -0,0 +1,45 @@
+package rules
+
+import (
+	"context"
+
+	sitter "github.com/tree-sitter/go-tree-sitter"
+	"go.lsp.dev/uri"
+)
+
+type Document struct {
+	fileUri uri.URI
+	source  []byte
+	version int32
+	parser  *Parser
+	cancel  context.CancelFunc
+}
+
+func NewDocument(fileUri uri.URI) *Document {
+	d := Document{
+		fileUri: fileUri,
+		source:  []byte{},
+		version: 0,
+		parser:  NewParser(),
+		cancel:  nil,
+	}
+	return &d
+}
+
+func (d *Document) UpdateSource(ctx context.Context, source []byte) error {
+	if d.cancel != nil {
+		d.cancel()
+	}
+
+	d.source = source
+	d.version += 1
+
+	parseCtx, cancel := context.WithCancel(ctx)
+	d.cancel = cancel
+
+	return d.parser.Parse(parseCtx, d.source)
+}
+
+func (d *Document) Query(pattern string) (*sitter.QueryMatches, error) {
+	return d.parser.Query(pattern, d.source)
+}
