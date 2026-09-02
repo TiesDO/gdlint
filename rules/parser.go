85a47ccf5263b818611b8d86cd00fdae92ca1529
Ties den Ouden <denouden.ties@gmail.com>
2026-09-02 22:26:59 +0200

    feat(rules): add parser struct to handle tree-sitter parsing logic

---
+++ rules/parser.go
@@ -0,0 +1,126 @@
+package rules
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+
+	gdscript "github.com/prestonknopp/tree-sitter-gdscript/bindings/go"
+	sitter "github.com/tree-sitter/go-tree-sitter"
+)
+
+type Parser struct {
+	parser   *sitter.Parser
+	language *sitter.Language
+	tree     *sitter.Tree
+}
+
+func NewParser() *Parser {
+	parser := Parser{
+		parser: sitter.NewParser(),
+	}
+
+	parser.language = sitter.NewLanguage(gdscript.Language())
+	parser.parser.SetLanguage(parser.language)
+
+	return &parser
+}
+
+func (p *Parser) Parse(ctx context.Context, source []byte) error {
+	tree := p.parser.ParseCtx(ctx, source, p.tree)
+
+	if tree == nil {
+		return errors.New("failed to parse tree")
+	}
+
+	p.tree = tree
+
+	return nil
+}
+
+func (p *Parser) String() string {
+	treeCursor := p.tree.Walk()
+	var builder strings.Builder
+
+	fprintTreeNode(treeCursor, 0, &builder)
+
+	return builder.String()
+}
+
+func (p *Parser) Query(pattern string, source []byte) (sitter.QueryMatches, error) {
+	query, qerr := sitter.NewQuery(p.language, pattern)
+
+	if qerr != nil {
+		return sitter.QueryMatches{}, fmt.Errorf("failed to create query: %v", qerr.Message)
+	}
+
+	cursor := sitter.NewQueryCursor()
+	matches := cursor.Matches(query, p.tree.RootNode(), source)
+
+	return matches, nil
+}
+
+func (p *Parser) SQuery(pattern string, source []byte) (string, error) {
+	var builder strings.Builder
+
+	query, qerr := sitter.NewQuery(p.language, pattern)
+
+	if qerr != nil {
+		return "", fmt.Errorf("failed to create query: %v", qerr.Message)
+	}
+
+	cursor := sitter.NewQueryCursor()
+	matches := cursor.Matches(query, p.tree.RootNode(), source)
+
+	for {
+		match := matches.Next()
+
+		if match == nil {
+			break
+		}
+
+		fmt.Fprintf(&builder, "match: %d\n", match.Id())
+		for _, capture := range match.Captures {
+			fmt.Fprintf(&builder, "  capture: %s (%d)\n", capture.Node.GrammarName(), capture.Index)
+			fmt.Fprintf(&builder, "  content: %s\n", source[capture.Node.StartByte():capture.Node.EndByte()])
+		}
+	}
+
+	output := builder.String()
+
+	if len(output) == 0 {
+		return "no matches found", nil
+	}
+
+	return output, nil
+}
+
+func fprintTreeNode(cursor *sitter.TreeCursor, depth int, builder *strings.Builder) {
+	indent := strings.Repeat("  ", depth)
+	node := cursor.Node()
+
+	prefix := ""
+	if fieldName := cursor.FieldName(); fieldName != "" {
+		prefix = fieldName + ": "
+	}
+
+	start := node.StartPosition()
+	end := node.EndPosition()
+
+	fmt.Fprintf(builder,
+		"%s%s%s [%d, %d] - [%d, %d]\n",
+		indent,
+		prefix,
+		node.GrammarName(),
+		start.Row, start.Column,
+		end.Row, end.Column)
+
+	if cursor.GotoFirstChild() {
+		fprintTreeNode(cursor, depth+1, builder)
+		for cursor.GotoNextSibling() {
+			fprintTreeNode(cursor, depth+1, builder)
+		}
+		cursor.GotoParent()
+	}
+}
---
+++ rules/parser_test.go
@@ -0,0 +1,75 @@
+package rules_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/TiesDO/gdlint/rules"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParserString(t *testing.T) {
+	parser := rules.NewParser()
+
+	err := parser.Parse(context.Background(), []byte("var x = 2"))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	result := parser.String()
+	expected := "source [0, 0] - [0, 9]\n  variable_statement [0, 0] - [0, 9]"
+
+	assert.Contains(t, result, expected)
+}
+
+func TestParserQuery(t *testing.T) {
+	parser := rules.NewParser()
+
+	source := []byte("var x = 2")
+	err := parser.Parse(context.Background(), source)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	matches, err := parser.Query("(variable_statement name: (name) @name)", source)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	matchCount := 0
+	for {
+		m := matches.Next()
+
+		if m == nil {
+			break
+		}
+
+		matchCount++
+	}
+
+	assert.Equal(t, matchCount, 1)
+}
+
+func TestParserSQuery(t *testing.T) {
+	parser := rules.NewParser()
+
+	source := []byte("var x = 2")
+	err := parser.Parse(context.Background(), source)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	result, err := parser.SQuery("(variable_statement name: (name) @name)", source)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "match: 0\n  capture: name (0)\n  content: x"
+
+	assert.Contains(t, result, expected)
+}
